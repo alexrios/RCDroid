bfd69c846e2aae37fbd8a0d86f8fe8535bc5a7f1
Daniel Passos <danpassos@gmail.com>
2012-07-26 22:22:08 -0300

    Refactor MainActivity

--- src/com/example/andorid/apis/mifare/MainActivity.java
+++ src/com/example/andorid/apis/mifare/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
-import android.graphics.Color;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.MifareClassic;
@@ -17,15 +16,15 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 import com.example.andorid.apis.mifare.task.ConsultaSaldoCartaoTask;
-import com.example.andorid.apis.mifare.util.HexaConverter;
 import com.example.andorid.apis.mifare.util.NumberUtil;
 
 import java.io.IOException;
 
 public class MainActivity extends Activity implements OnClickListener {
+
 	// UI Elements
-	private static TextView numeroCartao;
-	private static TextView saldoCartao;
+	private static TextView textViewNumeroCartao;
+	private static TextView textViewSaldoCartao;
 
 	// NFC parts
 	private static NfcAdapter mAdapter;
@@ -39,32 +38,14 @@ public class MainActivity extends Activity implements OnClickListener {
 	private static final int NETWORK = 4;
 	private static final String TAG = "purchtagscanact";
 	
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		numeroCartao = (TextView) findViewById(R.id.numeroCartao);
-		saldoCartao = (TextView) findViewById(R.id.saldo);
+        getComponents();
 		
-		numeroCartao.setTextColor(Color.BLACK);
-		saldoCartao.setTextColor(Color.BLACK);
-
-		// Capture Purchase button from layout
-		//Button scanBut = (Button) findViewById(R.id.clear_but);
-		// Register the onClick listener with the implementation above
-		//scanBut.setOnClickListener(this);
-
-		// Register the onClick listener with the implementation above
-		//scanBut.setOnClickListener(this);
-
 		mAdapter = NfcAdapter.getDefaultAdapter(this);
-		// Create a generic PendingIntent that will be deliver to this activity.
-		// The NFC stack
-		// will fill in the intent with the details of the discovered tag before
-		// delivering to
-		// this activity.
 		mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
 				getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 		// Setup an intent filter for all MIME based dispatches
@@ -78,102 +59,94 @@ public class MainActivity extends Activity implements OnClickListener {
 		mFilters = new IntentFilter[] { ndef, };
 
 		// Setup a tech list for all NfcF tags
-		mTechLists = new String[][] { new String[] { MifareClassic.class
-				.getName() } };
+		mTechLists = new String[][] { new String[] { MifareClassic.class.getName() } };
 
 		Intent intent = getIntent();
 		resolveIntent(intent);
-
-	}
-	
-	public void consultarSaldoWebService(TextView block_0_Data2, String numeroChip) {
-        ConsultaSaldoCartaoTask consultaSaldoCartaoTask = new ConsultaSaldoCartaoTask(this, block_0_Data2, numeroChip);
-		consultaSaldoCartaoTask.execute();
 	}
 
-	void resolveIntent(Intent intent) {
+    private void getComponents() {
+        textViewNumeroCartao = (TextView) findViewById(R.id.numeroCartao);
+        textViewSaldoCartao = (TextView) findViewById(R.id.saldo);
+    }
+
+    @Override
+   	public void onResume() {
+   		super.onResume();
+   		mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
+   	}
+
+   	@Override
+   	public void onPause() {
+   		super.onPause();
+   		mAdapter.disableForegroundDispatch(this);
+   	}
+
+    @Override
+   	public void onNewIntent(Intent intent) {
+   		Log.i("Foreground dispatch", "Discovered tag with intent: " + intent);
+   		resolveIntent(intent);
+   	}
+
+	public void onClick(View v) {
+		clearFields();
+	}
+
+	private void resolveIntent(Intent intent) {
 		// Parse the intent
 		String action = intent.getAction();
 		if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
 			// status_Data.setText("Discovered tag with intent: " + intent);
 			Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
 			MifareClassic mfc = MifareClassic.get(tagFromIntent);
-			byte[] data;
 			byte[] uid;
 			try {
 				mfc.connect();
 				uid = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
-				String uidHex = HexaConverter.getHexString(uid, uid.length);
 				long numeroChipCartao = NumberUtil.bigToLittleEndian(uid);
-				this.numeroCartao.setText(Long.toString(numeroChipCartao));
-				consultarSaldoWebService(saldoCartao, Long.toString(numeroChipCartao));
-			
+				this.textViewNumeroCartao.setText(Long.toString(numeroChipCartao));
+				consultarSaldoWebService(textViewSaldoCartao, Long.toString(numeroChipCartao));
 			} catch (IOException e) {
 				Log.e(TAG, e.getLocalizedMessage());
 				showAlert(NETWORK);
 			}
-		} else {
 		}
 	}
+
+    private void consultarSaldoWebService(TextView textViewSaldo, String numeroChip) {
+        ConsultaSaldoCartaoTask consultaSaldoCartaoTask = new ConsultaSaldoCartaoTask(this, textViewSaldo, numeroChip);
+   		consultaSaldoCartaoTask.execute();
+   	}
 
 	private void showAlert(int alertCase) {
-		// prepare the alert box
-		AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+        String message = "";
 		switch (alertCase) {
-
-		case AUTH:// Card Authentication Error
-			alertbox.setMessage("Authentication Failed on Block 0");
-			break;
-		case EMPTY_BLOCK_0: // Block 0 Empty
-			alertbox.setMessage("Failed reading Block 0");
-			break;
-		case EMPTY_BLOCK_1:// Block 1 Empty
-			alertbox.setMessage("Failed reading Block 0");
-			break;
-		case NETWORK: // Communication Error
-			alertbox.setMessage("Tag reading error");
-			break;
+            case AUTH:              // Card Authentication Error
+                message = "Authentication Failed on Block 0";
+                break;
+            case EMPTY_BLOCK_0:     // Block 0 Empty
+                message = "Failed reading Block 0";
+                break;
+            case EMPTY_BLOCK_1:     // Block 1 Empty
+                message = "Failed reading Block 0";
+                break;
+            case NETWORK:           // Communication Error
+                message = "Tag reading error";
+                break;
 		}
-		// set a positive/yes button and create a listener
-		alertbox.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 
-			// Save the data from the UI to the database - already done
-			public void onClick(DialogInterface arg0, int arg1) {
-				clearFields();
-			}
-		});
-		// display box
-		alertbox.show();
-
-	}
-
-	public void onClick(View v) {
-		clearFields();
+        new AlertDialog.Builder(this)
+            .setMessage(message)
+            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    clearFields();
+                }
+            }).show();
 	}
 
 	private static void clearFields() {
-		numeroCartao.setText("");
-		saldoCartao.setText("");
+		textViewNumeroCartao.setText("");
+		textViewSaldoCartao.setText("");
 	}
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters,
-				mTechLists);
-	}
-
-	@Override
-	public void onNewIntent(Intent intent) {
-		Log.i("Foreground dispatch", "Discovered tag with intent: " + intent);
-		resolveIntent(intent);
-		// mText.setText("Discovered tag " + ++mCount + " with intent: " +
-		// intent);
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-		mAdapter.disableForegroundDispatch(this);
-	}
 }
